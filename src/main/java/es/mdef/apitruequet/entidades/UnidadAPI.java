e411cbca0764e1b7e6f161fad52b1797f548c1f9
samotcarrasco <samotcarrasco@gmail.com>
2023-06-05 23:28:26 +0200

    unidad abstract

--- src/main/java/es/mdef/apitruequet/entidades/UnidadAPI.java
+++ src/main/java/es/mdef/apitruequet/entidades/UnidadAPI.java
@@ -2,7 +2,7 @@ package es.mdef.apitruequet.entidades;
 
 import es.mde.acing.utils.UnidadImpl;
 
-public class UnidadAPI extends UnidadImpl{
+public abstract class UnidadAPI extends UnidadImpl{
 	 
 	Long id;
 
